0e187ba5f10c22d44dcc07f22dffba9e4a4ab4ae
Patticatti <139997703+Patticatti@users.noreply.github.com>
2024-01-21 08:21:52 -0800

    added search bar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import RedirectButton from './RedirectButton.tsx';
+import MenuSearchBar from './MenuSearchBar.tsx';
 
-const NavBar = () => {
-    const navigate = useNavigate();
-  
-    const handleViewBuildingClick = () => {
-      navigate("/");
-    };
+const NavBar = (props) => {
     return (
         <nav>
             <RedirectButton path='/' text="StudyScout" type="home" />
+            {props.search && <MenuSearchBar query="Irving K. Barber Learning Centre, 1961 E Mall, Vancouver, BC V6T 1Z1"onClickPath='/main'/>}
             <RedirectButton text="Sign in" type="login"/>
         </nav>
     );
--- frontend/src/pages/Listings.jsx
+++ frontend/src/pages/Listings.jsx
@@ -6,17 +6,19 @@ import MainSection from '../components/MainSection.tsx';
 export default function Main() {
     return (
         <>
-            <NavBar/>
+            <NavBar search={true}/>
             <div id="background">
-                <img id="backgroundImg"/>
                 <SideBar/>
-                <MainSection/>
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2603.4678210311436!2d-123.25525762373182!3d49.26753207139053!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548672b6e7ecb487%3A0xb469afb686ebb577!2sIrving%20K.%20Barber%20Learning%20Centre!5e0!3m2!1sen!2sca!4v1705832961155!5m2!1sen!2sca"
-                    allowFullScreen=""
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
+                <div>
+                   <MainSection/>
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2603.4678210311436!2d-123.25525762373182!3d49.26753207139053!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548672b6e7ecb487%3A0xb469afb686ebb577!2sIrving%20K.%20Barber%20Learning%20Centre!5e0!3m2!1sen!2sca!4v1705832961155!5m2!1sen!2sca"
+                        allowFullScreen=""
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe> 
+                </div>
+                
             </div>
         </>
     );
