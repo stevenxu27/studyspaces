8c8b6776d6db47474ac5c065f10cb1c7b8f94dd6
Patticatti <139997703+Patticatti@users.noreply.github.com>
2024-01-21 07:20:09 -0800

    finished styling

--- frontend/src/components/MainFloor.tsx
+++ frontend/src/components/MainFloor.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function MainFloor() {
   return (
     <div id="mainFloor">
-      <span class="mainSpan">
+      <span className="mainSpan">
           <img className="icon" src="./images/arrow-prev-svgrepo-com.svg"/>
-          <p>Irving K Barber Building</p>
+          <h4>Irving K Barber Building</h4>
           <div className="darkShade"/>
       </span>
       <div id="mappedInEmbed">
@@ -19,10 +19,14 @@ export default function MainFloor() {
           <div className="infoDescription">
             <div className="locationDesc">
                 <img className="icon" src="./images/location-pin-svgrepo-com.svg"/>
-                <h6>700m</h6>
+                <h6>700m away</h6>
             </div>
             <h3>Irving K Barber</h3>
             <p>The Irving K. Barber Learning Centre is a facility at the Vancouver campus of the University of British Columbia. The learning centre...</p>
+            <h4>Open parties:</h4>
+            <img className="partyImg" src="./images/viewBuildings/open parties.png"/>
+            <h4>View recent reviews:</h4>
+            <img className="partyImg" src="./images/viewBuildings/recentreviews.png"/>
           </div>
         </div>
         
