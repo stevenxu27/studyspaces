7eccb1f4fb1e8bb901d155100898528450f44a63
Patticatti <139997703+Patticatti@users.noreply.github.com>
2024-01-21 07:04:58 -0800

    mappedinEmbed added in viewbuilding page

--- frontend/src/components/MainFloor.tsx
+++ frontend/src/components/MainFloor.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 export default function MainFloor() {
   return (
     <div id="mainFloor">
-      <div id="mappedInEmbed">
-        <span>
-          <img className="backIcon" src="./images/arrow-prev-svgrepo-com.svg"/>
+      <span class="mainSpan">
+          <img className="icon" src="./images/arrow-prev-svgrepo-com.svg"/>
           <p>Irving K Barber Building</p>
-        </span>
+          <div className="darkShade"/>
+      </span>
+      <div id="mappedInEmbed">
       </div>
       <div id="infoContainer">
         <div className="infoContainerLeft">
@@ -16,11 +17,11 @@ export default function MainFloor() {
         </div>
         <div className="infoContainerRight">
           <div className="infoDescription">
-            <span className="locationDesc">
+            <div className="locationDesc">
                 <img className="icon" src="./images/location-pin-svgrepo-com.svg"/>
-                <h6>700m away</h6>
-            </span>
-            <h4>Irving K Barber</h4>
+                <h6>700m</h6>
+            </div>
+            <h3>Irving K Barber</h3>
             <p>The Irving K. Barber Learning Centre is a facility at the Vancouver campus of the University of British Columbia. The learning centre...</p>
           </div>
         </div>
