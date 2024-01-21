2ebf88cc8b7e6638091553f64e4dded4d0297db6
Patticatti <139997703+Patticatti@users.noreply.github.com>
2024-01-20 23:49:00 -0800

    i keep forgetting to commit (listing compoenet)

--- frontend/src/components/ListingComponent.tsx
+++ frontend/src/components/ListingComponent.tsx
@@ -9,9 +9,10 @@ export default function ListingComponent() {
                 <img className="icon" src="./images/location-pin-svgrepo-com.svg"/>
                 <h6>700m away</h6>
             </span>
-            <h3>Irving K Barber Building</h3>
+            <h4>Irving K Barber Building</h4>
             <p>The Irving K. Barber Learning Centre is a facility at the Vancouver campus of the University of British Columbia. The learning centre...</p>
             <button className="listingButton">
+                <h6>Book now</h6>
                 <img className="icon" src="./images/arrow-right-svgrepo-com.svg"/>
             </button>
         </div>
