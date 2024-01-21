fb9886ca341eec1d2e14edf8e72c4c1175afea1c
Patticatti <139997703+Patticatti@users.noreply.github.com>
2024-01-20 17:17:00 -0800

    react router dom added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from './Landing';
+// import About from './About';
+// import Contact from './Contact';
 
-export default function App() {
-    return (
-        <div>
-            <h1>Hello, React!</h1>
-        </div>
-    );
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        {/* <Route path="/search" component={Search} />
+        <Route path="/view-building" component={ViewBuilding} /> */}
+      </Switch>
+    </Router>
+  );
 }
+
+export default App;
