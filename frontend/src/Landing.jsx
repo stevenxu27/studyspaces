73bc25365edb7d06a637fd44d2e96401c03a24bb
Patticatti <139997703+Patticatti@users.noreply.github.com>
2024-01-20 17:51:19 -0800

    delete old frontend folder

--- frontend/src/Landing.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Landing() {
-    return (
-        <div id="titleSection">
-            <LandingTitle title="Welcome to My App" desc="Find study spaces from the comfort of your home."/>
-        </div>
-    );
-}
--- frontend/src/components/NavBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function NavBar() {
-    return (
-        <nav className="navBar">
-            <h1>Hello, React!</h1>
-        </nav>
-    );
-}
